1c07d5b3d0cdd1ae86ba28fcd1ab4e6916a921bd
Karol Szpakowski <karol.szpakowski@gmail.com>
2023-04-15 10:08:12 +0200

    initial

---
+++ poll-service/src/test/java/com/kszpakowski/polls/pollservice/PollServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kszpakowski.polls.pollservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PollServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
